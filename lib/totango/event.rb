e65d2afaf92e7533d21efd75f467fd7ff1bcf067
Greg Oleksiak <greg.oleksiak@gmail.com>
2017-01-20 08:41:57 -0600

    Add support for updating Account and User attributes
    Signed-off-by: Greg Oleksiak <greg.oleksiak@gmail.com>

--- lib/totango/event.rb
+++ lib/totango/event.rb
@@ -9,7 +9,19 @@ module Totango
     # The HTTP API can be used to track user-activity from anywhere in your web-app.
     # Simply send the following parametrized HTTP request every time a user performs an important user-action.
 
-    attr_accessor :service_id, :username, :account_id, :account_name, :activity, :module
+    USER_PARAM_SUFFIX = 'u'.freeze
+    ACCOUNT_PARAM_SUFFIX = 'o'.freeze
+
+    attr_accessor(
+      :service_id,
+      :username,
+      :account_id,
+      :account_name,
+      :activity,
+      :module,
+      :user_attributes,
+      :account_attributes
+    )
 
     def initialize(options)
       options.each do |option_name, option_value|
@@ -25,7 +37,15 @@ module Totango
         :sdr_odn => account_name, # A human readable name for the account (will be used on Totango’s UI and reports)
         :sdr_a => activity, # Name of the activity the user performed
         :sdr_m => self.module, # Name of the module the user is using within the application
-      }
+      }.merge!(
+        object_attributes(user_attributes, USER_PARAM_SUFFIX).merge!(
+          object_attributes(account_attributes, ACCOUNT_PARAM_SUFFIX)
+        )
+      )
+    end
+
+    def object_attributes(attributes, suffix)
+      attributes.each_with_object({}) { |i, h| h.merge!({ "sdr_#{suffix}.#{i[0].to_s}".to_sym => i[1] }) }
     end
 
     def to_url
